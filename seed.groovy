53ee7c574cbe6efaa66a6e4b881135c454289708
Sauli Kukkonen <sauli.kukkonen@q-factory.fi>
2018-06-10 22:32:52 +0300

    new addition

--- seed.groovy
+++ seed.groovy
@@ -74,3 +74,31 @@ pipelineJob('TestiJobi3') {
     }
     }
 }
+
+pipelineJob('TestiJobi4') {
+    definition {
+        cps {
+            script(readFileFromWorkspace('Jenkinsfile_second'))
+            sandbox()
+        }
+    
+  logRotator {
+    numToKeep(10)
+    artifactNumToKeep(10)
+    }   
+    
+       parameters {
+       stringParam('target', 'Demo', 'target build')
+       stringParam('label', 'kone1', 'label used in build env')
+       stringParam('branch', 'master', 'used branch')
+       stringParam('preparationCommands', 'source /opt/poky-st/2.2.1/environment-setup-cortexa7hf-neon-poky-linux-gnueabi', 'command for script')
+       stringParam('dir', 'testihakemisto', 'build dir')
+     }
+  
+  }     
+    triggers {
+     triggers {
+        scm('H/15 * * * *')
+    }
+    }
+}
